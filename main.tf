0f9255a0b279e8ac700762d541741b3a4ed718c3
qkrwjdan <madogisa12@naver.com>
2022-08-21 01:48:57 +0900

    [feat] vpc and subnet

--- main.tf
+++ main.tf
@@ -2,12 +2,31 @@ provider "aws" {
   region     = "ap-northeast-2"
 }
 
-resource "aws_instance" "my_first_server" {
+resource "aws_instance" "terraform-ec2-server" {
   ami           = "ami-0ea5eb4b05645aa8a"
   instance_type = "t2.micro"
   tags = {
-    Name = "ubuntu"
+    Name = "terraform-ec2-server"
   }
+}
+
+resource "aws_vpc" "terraform-vpc" {
+  cidr_block = "10.0.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    "Name" = "terraform-vpc"
+  }
+}
+
+resource "aws_subnet" "terraform-subnet-public-01" {
+  vpc_id = aws_vpc.terraform-vpc.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    "Name" = "terraform-subnet-public-01"
+  }
+  
 }
 
 # resource "<provider>_<resource>" "name" {
